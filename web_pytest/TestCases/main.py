5a74351b32473f24c47fe2058907bf184f7f015e
gehongming <1010562639@qq.com>
2019-11-22 10:34:34 +0800

    file文件获取

--- web_pytest/TestCases/main.py
+++ web_pytest/TestCases/main.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+import sys
+sys.path.append('./')  #获取project 根目录,jenkits执行需要
 # pytest.main(["-v","-m","login"])
 #
 # pytest.main(["-v","-m","demo","--reruns","1","--alluredir=../OutPuts/allure-results"])
