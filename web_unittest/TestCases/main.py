5bbccec601409d01e426de58562aa30d254377cb
葛红明 <gehm@ti-net.com.cn>
2021-11-03 16:22:20 +0800

    优化2021.11.3

--- web_unittest/TestCases/main.py
+++ web_unittest/TestCases/main.py
@@ -1,22 +1,52 @@
-import sys
-
-sys.path.append('./')  #获取project 根目录,jenkits执行需要
-# print(sys.path)
-import HTMLTestRunnerNew
 import unittest
-from web_unittest.common import contants
-import time
+from unittestreport import TestRunner
 
-discover = unittest.defaultTestLoader.discover(contants.case_dir, pattern='test*.py')
-curTime = time.strftime("%Y-%m-%d %H%M", time.localtime())
-with open(contants.reports_html+'/api_{}.html'.format(curTime), 'wb+') as files:
-            runner = HTMLTestRunnerNew.HTMLTestRunner(
-                stream=files,
-                verbosity=1,
-                title='20190411py15测试报告',
-                description='UI测试报告',
-                tester='冥鴻')
-            runner.run(discover)
+from common.config import *
+from common.chose_case import ChoseCase
+from common.handle_path import CASE_DIR, REPORT_DIR
+from common.send_email import send_email
+import sys
+sys.path.append('/')
+
+input = sys.argv[-1]
+DIR = ChoseCase().chosedir(input)
+
+# 创建测试套件
+suite = unittest.TestSuite()
+
+# 加载用例到套件
+loader = unittest.TestLoader()
+# suite.addTest(loader.discover(CASE_DIR))
+suite.addTest(loader.discover(DIR))
+
+runner = TestRunner(suite,
+                    filename=f"clink2_{input}.html",
+                    report_dir=REPORT_DIR,
+                    title=f'clink2_{input}测试报告',
+                    tester='demo',
+                    desc=f"clink2_{input}接口测试报告",
+                    templates=1)
+runner.run()
+
+# 发送钉钉通知
+runner.dingtalk_notice(url=config.get('dingding','url'),
+                       key=config.get('dingding','key'),
+                       secret=config.get('dingding','secret'))
+
+runner.send_email(
+    host=config.get('email', 'host'),
+    port=465,
+    user=config.get('email', 'user'),
+    password=config.get('email', 'password'),
+    to_addrs=["gehm@ti-net.com.cn"])
+
+
+
+# 多线程
+# runner.run(thread_count=5)
+# 执行测试用例，失败重运行设置为3次，间隔时间为2秒
+# runner.rerun_run(count=3, interval=2)
+
 
 
 
